79d4701e20f2dc3a2e93933872c3393f2fd15343
Paul Zabelin <paulz@users.noreply.github.com>
2019-06-21 22:46:05 +0100

    add universal framework

---
+++ Universal/Universal.h
@@ -0,0 +1,19 @@
+//
+//  Universal.h
+//  Universal
+//
+//  Created by Paul Zabelin on 6/21/19.
+//  Copyright © 2019 Paul Zabelin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Universal.
+FOUNDATION_EXPORT double UniversalVersionNumber;
+
+//! Project version string for Universal.
+FOUNDATION_EXPORT const unsigned char UniversalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Universal/PublicHeader.h>
+
+
